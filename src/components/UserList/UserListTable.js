3fafba2ccedb7b578810cb4b06ead63d97af7752
IamSoPradaYou <9999goldcoin@gmail.com>
2020-10-05 20:42:50 +0300

    Responsive styles

--- src/components/UserList/UserListTable.js
+++ src/components/UserList/UserListTable.js
@@ -24,23 +24,23 @@ const UserListTable = ({ users, find }) => {
                 />
             </Helmet>
             <div className={styles.container}>
-            <h2>Поиск по username:</h2>
+                <h2>Поиск по username:</h2>
 
                 <div className={styles.users__table}>
-                    
+
                     <div className={styles.users__form}>
                         <input className={styles.users__form__input} type="text" onChange={e => setFindUsername(e.target.value)} />
                         <button className={styles.users__form__button} onClick={() => find(findUsername)}>Найти</button>
                     </div>
 
-                    <table className={styles.table}>
+                    <table align="center" className={styles.table}>
                         <thead >
                             <tr >
-                                <th className={styles.table__head}>#</th>
-                                <th className={styles.table__head}>ID</th>
-                                <th className={styles.table__head}>Username</th>
-                                <th className={styles.table__head}>Имя</th>
-                                <th className={styles.table__head}>Фамилия</th>
+                                <th scope="col" className={styles.table__head}>#</th>
+                                <th scope="col" className={styles.table__head}>ID</th>
+                                <th scope="col" className={styles.table__head}>Username</th>
+                                <th scope="col" className={styles.table__head}>Имя</th>
+                                <th scope="col" className={styles.table__head}>Фамилия</th>
                             </tr>
                         </thead>
                         <tbody className="table__text">
@@ -49,11 +49,11 @@ const UserListTable = ({ users, find }) => {
                                     const { id, username, first_name, last_name } = user
                                     return (
                                         <tr key={id}>
-                                            <td className={styles.table__row}>{idx + 1}</td>
-                                            <td className={styles.table__row}>{id}</td>
-                                            <td className={styles.table__row}>{username}</td>
-                                            <td className={styles.table__row}>{first_name}</td>
-                                            <td className={styles.table__row}>{last_name}</td>
+                                            <td data-label="#" className={styles.table__row}>{idx + 1}</td>
+                                            <td data-label="ID" className={styles.table__row}>{id}</td>
+                                            <td data-label="Username" className={styles.table__row}>{username}</td>
+                                            <td data-label="Имя" className={styles.table__row}>{first_name}</td>
+                                            <td data-label="Фамилия" className={styles.table__row}>{last_name}</td>
                                         </tr>
                                     )
                                 })
