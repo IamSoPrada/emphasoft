95fda5251746c5839f5f26683ac2973133a41834
IamSoPrada <9999goldcoin@gmail.com>
2020-10-14 09:33:53 +0300

    upd

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Link, Redirect } from "react-router-dom"
+import { Link } from "react-router-dom"
 import styles from "./HomePage.module.css"
 
 
 const HomePage = () => {
-    if(localStorage.getItem("users")){
-      return  <Redirect to='/users' />
-    }
+
     return (
         <div className={styles.menu}>
             <Link className={styles.menu__link} to="/users">Загрузить пользователей</Link>
